2ebbcf21e7e38728d840b417d65f1b469fcc184c
Siyuan Zeng <siyuanzeng@gmail.com>
2017-01-09 10:04:56 -0500

    a

--- src/zeng/siyuan/onceaday/link/onceaday/How2Forgert.java
+++ src/zeng/siyuan/onceaday/link/onceaday/How2Forgert.java
@@ -286,7 +286,7 @@ public class How2Forgert implements Serializable {
                 }
 //            }
 //        }
-//        Collections.sort(tasks, new Task());
+        if(null != tasks && tasks.size()>2) Collections.sort(tasks, new Task());
     }
 
 
