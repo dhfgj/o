4763ce0d34a1801e68805dd4ba984cd0895ac330
Owner <siyuanzeng@hotmail.com>
2017-01-21 20:07:04 -0800

    1

--- src/zeng/siyuan/button/asdfSAFASDFASDFASDFASDFASFGKJHXZCLKHVLKASJHDFKJASHKVBZXKJFGA.java
+++ src/zeng/siyuan/button/asdfSAFASDFASDFASDFASDFASFGKJHXZCLKHVLKASJHDFKJASHKVBZXKJFGA.java
@@ -1,17 +1,17 @@
 package zeng.siyuan.button;
 
-import com.epam.preprod.firefox.model.KFHASDF;
+//import com.epam.preprod.firefox.model.KFHASDF;
 
 /**
  * Created by SiyuanZeng's on 12/17/2016.
  */
 public class asdfSAFASDFASDFASDFASDFASFGKJHXZCLKHVLKASJHDFKJASHKVBZXKJFGA {
     public static void main(String[] args) {
-        KFHASDF KFHASDF = new KFHASDF();
-        try {
-            KFHASDF.testOpenLogin3();
-        } catch (InterruptedException e1) {
-            e1.printStackTrace();
-        }
+//        KFHASDF KFHASDF = new KFHASDF();
+//        try {
+//            KFHASDF.testOpenLogin3();
+//        } catch (InterruptedException e1) {
+//            e1.printStackTrace();
+//        }
     }
 }
